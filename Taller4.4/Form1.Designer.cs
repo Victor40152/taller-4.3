dccc7c1373f740b5214219af0cfb817f8badee4d
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-03 20:44:50 -0500

    Taller de las edades

---
+++ Taller4.4/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Taller4._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.btntotal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Programa para Calcular la edad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese su Edad :";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(269, 168);
+            this.txtedad.Multiline = true;
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(121, 24);
+            this.txtedad.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Total :";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal.Location = new System.Drawing.Point(158, 306);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(22, 24);
+            this.lbltotal.TabIndex = 4;
+            this.lbltotal.Text = "0";
+            // 
+            // btntotal
+            // 
+            this.btntotal.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntotal.Location = new System.Drawing.Point(452, 163);
+            this.btntotal.Name = "btntotal";
+            this.btntotal.Size = new System.Drawing.Size(90, 34);
+            this.btntotal.TabIndex = 5;
+            this.btntotal.Text = "Enviar";
+            this.btntotal.UseVisualStyleBackColor = true;
+            this.btntotal.Click += new System.EventHandler(this.btntotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btntotal);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Button btntotal;
+    }
+}
+
---
+++ Taller4.4/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller4._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btntotal_Click(object sender, EventArgs e)
+        {
+            double edad, total;
+            edad = double.Parse(txtedad.Text);
+            total = (2019 - edad);
+            lbltotal.Text = total.ToString();
+            if (total >= 18)
+            {
+                MessageBox.Show("Es mayor de edad");
+            
+            }
+            else
+            {
+                MessageBox.Show("Es menor de edad");
+
+            }
+        }
+
+    }
+}
